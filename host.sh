792ec158b0da41c82f802afb9e65e1c02b41b9ba
swamy527 <swamybandari527@gmail.com>
2024-09-26 18:29:35 +0530

    instance file added

--- host.sh
+++ host.sh
@@ -4,7 +4,7 @@ instance=("web", "cart", "user", "redis")
 
 for i in ${instance[@]}; do
 
-    if [ $i == "web" ] || [ $i == "cart" ] || [ $i == "redis" ]; then
+    if [ [ $i == "web" ] || [ $i == "cart" ] || [ $i == "redis" ] ]; then
         echo -e "\n correctly printed $i"
     else
         echo -e "\n printed for $i"
