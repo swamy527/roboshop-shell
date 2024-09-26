1ca0b1bdfd6da01082ab5ef9697d44de707dc6a8
swamy527 <swamybandari527@gmail.com>
2024-09-26 17:31:01 +0530

    instance file added

---
+++ instances.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+ID=$(id -u)
+Timestamp=$(date +%F-%T)
+Logs=/tmp/log.$Timestamp
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+domain="beesh.life"
+Host_zone=Z0065515281TOZ02X40CA
+instance=("web", "catalogue", "mongodb", "shipping", "user", "cart", "redis", "mysql")
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "\n$R $2 is failed $N \n"
+        exit 1
+    else
+        echo -e "\n$G $2 is success $N \n"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+   echo -e "\n$R Run script as root user $N \n"
+   exit 50
+else
+   echo -e "\n$G you are root user $N \n"
+fi
+
+
+for i in ${instance[@]}
+do
+  echo -e "\n launching $i " 
+  if [ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]
+  then
+      INSTANCE_TYPE="t2.medium"
+  else
+      INSTANCE_TYPE="t2.micro"
+  fi
+  IP_address=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d86841764a147f28 --tag-specifications "ResourceType=instance, Tags= [{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+  
+  aws route53 change-resource-record-sets \		
+  --hosted-zone-id $Host_zone \		
+  --change-batch '		
+  {		
+    "Comment": "Testing creating a record set"		
+    ,"Changes": [{		
+      "Action"              : "CREATE"		
+      ,"ResourceRecordSet"  : {		
+        "Name"              : "'$i'.'$domain'"		
+        ,"Type"             : "A"		
+        ,"TTL"              : 1		
+        ,"ResourceRecords"  : [{		
+            "Value"         : "'$IP_address'"		
+        }]		
+      }		
+    }]		
+  }		
+   '
+done
+
