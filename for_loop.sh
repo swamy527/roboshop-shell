7cd6a9f9af7018cbeafe70d4b3883de88772d415
swamy527 <swamybandari527@gmail.com>
2024-10-02 19:48:44 +0530

    file added

--- for_loop.sh
+++ for_loop.sh
@@ -2,7 +2,7 @@
 
 instance=("web" "catalogue" "mongodb" "shipping" "user" "cart" "redis" "mysql" "payment" "rabbitmq")
 
-for i in ${instance[@]}
+for i in "${instance[@]}"
 do
-ansible-playbook -e component=$i main.yaml
+ansible-playbook -e component="$i" main.yaml
 done
