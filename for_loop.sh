a4c177817ee7d1dce2a20b630b84256fbbc4471c
swamy527 <swamybandari527@gmail.com>
2024-10-04 08:09:53 +0530

    file updated

--- for_loop.sh
+++ for_loop.sh
@@ -2,6 +2,12 @@
 
 instance=("mongodb" "redis" "rabbitmq" "mysql" "user" "cart" "catalogue" "shipping" "payment" "web")
 
+cd /tmp
+
+git clone https://github.com/swamy527/roboshop-ansible-roles.git
+
+cd /tmp/roboshop-ansible-roles
+
 for i in "${instance[@]}"
 do
 ansible-playbook -e component="$i" main.yaml
